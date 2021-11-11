9c8873531d2d08164a054d41081945a190790d9a
Marlon Pereira <marloncristian@gmail.com>
2021-11-11 12:13:24 +0100

    updating

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const request = require('request')
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const client_id = '505748'
 const client_secret = 'QGwbd45wo8kodrGURxlBvGhQCV3Bm9kgTn2dZX7TCbElrojHlh'
 const state = 'notimportant'
 const version = 1
+const fetch = require('node-fetch')
 
 console.log(`version: ${version}`)
 
@@ -13,7 +16,18 @@ var fdefget = (_req, res) => {
 };
 
 var fauth = function (req, res) {
-	res.send(`code: ${req.query.code}`)
+  let todo = {
+    code : req.query.code, 
+    client_id : 505748,
+    client_secret : 'QGwbd45wo8kodrGURxlBvGhQCV3Bm9kgTn2dZX7TCbElrojHlh',
+    grant_type : authorization_code
+  };
+  fetch('https://api.officient.io/1.0/token', {
+    method: 'POST',
+    body: JSON.stringify(todo),
+    headers: { 'Content-Type': 'application/json' }
+}).then(rsp => rsp.json())
+  .then(json => res.send(json));
 }
 
 var app = express()
